3c1e4b9fa85f7ec42949a163eb6bc7de1d9c72e7
Ilham Akbar <ilhamije@gmail.com>
2023-10-31 19:00:46 +0700

    use typescript react

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,14 @@
+// src/components/LandingPage.tsx
+import React from 'react';
+// import './LandingPage.css';
+
+const LandingPage: React.FC = () => {
+    return (
+        <div className="landing-page">
+            <h1>Welcome to Progress Drone</h1>
+            <p>Track Your Dream House Progress.</p>
+        </div>
+    );
+}
+
+export default LandingPage;
